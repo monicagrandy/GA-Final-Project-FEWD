920a1d722c30beeaa3b61ecf6442862c2eec9328
Monica Grandy <monica.grandy@gmail.com>
2015-06-18 09:55:02 -0700

    initial commit

---
+++ admissionChallengeMKS.js
@@ -0,0 +1,14 @@
+var myArray = ["Monica Grandy", "mg10488"];
+
+var cutName = function(string){
+	string.split();
+	return string;
+};
+
+var myData = {};
+
+myData.fullName = cutName(myArray[0]);
+myData.skype = myArray[1];
+myData.github = "monicagrandy";
+
+
